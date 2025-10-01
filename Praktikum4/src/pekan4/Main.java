c510eb340a4b723a78c6a7c9766192c7c17f473b
Joovzx <suryaniwati07@gmail.com>
2025-10-01 09:20:44 +0700

    Pekan 4

---
+++ Praktikum4/src/pekan4/Main.java
@@ -0,0 +1,33 @@
+package pekan4;
+
+public class Main {
+	public static void main(String[] args) {
+		Book novel = new Novel("Laskar Pelangi", "Andrea Hirata", "Drama");
+		Book magazine = new Magazine("National Geographic", "Various Authors", "Science");
+		Book textbook = new Textbook("Pemrograman Java", "Anonimous", "Informatika");
+		
+		User user = new User();
+		
+		System.out.println("=== Detail Buku ===");
+		user.viewBookDetails(novel);
+		System.out.println();
+		user.viewBookDetails(magazine);
+		System.out.println();
+		user.viewBookDetails(textbook);
+		System.out.println();
+		
+		System.out.println("=== Proses Peminjaman Buku ===");
+		user.borrowBook(novel);
+		user.borrowBook(magazine);
+		
+		System.out.println("\nStatus Buku Setelah Dipinjam");
+		System.out.println(novel.getTitle() + " tersedia: " + novel.isAvailable());
+		System.out.println(magazine.getTitle() + " tersedia: " + magazine.isAvailable());
+		
+		System.out.println("=== Proses Pengembalian Buku ===");
+		user.returnBook(novel);
+		
+		System.out.println("\nStatus Buku Setelah Dikembalikan");
+		System.out.println(novel.getTitle() + " tersedia: " + novel.isAvailable());
+	}
+}
